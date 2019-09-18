fcbaeff1faf36267b37994ebeafc0703eb8ebfda
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-18 20:19:41 +0300

    Unnecessary lines deleted in MoviesList component

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -14,27 +14,20 @@ class MoviesList extends React.Component {
   componentDidMount() {
     if ("location" in this.props) {
       const values = queryString.parse(this.props.location.search);
-      //console.log(values.query);
       this.props.fetchMovies(values.query);
       window.scrollTo(0, 0);
     } else {
-      //console.log("searchTerm is: ", this.props.searchTerm);
       const values = queryString.parse(this.props.searchTerm);
-      //console.log(values.query);
       switch (values.query) {
         case "trendingMovies":
-          //console.log("trendingMovies case fired");
           this.props.fetchTrendingMovies();
           window.scrollTo(0, 0);
           break;
         case "upcomingMovies":
-          console.log("upcomingMovies case fired");
           this.props.fetchUpcomingMovies();
           window.scrollTo(0, 0);
           break;
         default:
-          console.log("default case fired");
-          console.log(values.query);
           this.props.fetchMovies(values.query);
           window.scrollTo(0, 0);
       }
@@ -44,32 +37,21 @@ class MoviesList extends React.Component {
   componentDidUpdate(prevProps) {
     if ("location" in this.props) {
       if (this.props.location.search !== prevProps.location.search) {
-        console.log("componentDidUpdate invoked,", this.props.location.search);
         const values = queryString.parse(this.props.location.search);
-        console.log(values.query);
         this.props.fetchMovies(values.query);
         window.scrollTo(0, 0);
       }
     } else {
       if (this.props.searchTerm !== prevProps.searchTerm) {
-        console.log("componentDidUpdate invoked,", this.props.location.search);
         const values = queryString.parse(this.props.searchTerm);
-        console.log(values.query);
         this.props.fetchMovies(values.query);
         window.scrollTo(0, 0);
       }
     }
   }
 
-  // handleClick = id => {
-  //   console.log(id);
-  //   this.props.selectedMovieId(id);
-  // };
-
   render() {
-    //console.log(this.props);
     if ("location" in this.props) {
-      console.log("location: ", this.props.location);
       const { errorMessage, moviesData } = this.props;
       const { search } = this.props.location;
 
@@ -93,12 +75,10 @@ class MoviesList extends React.Component {
         );
       }
     } else {
-      //console.log("searchTerm: ", this.props.searchTerm);
-
       const searchArray = ["trendingMovies", "upcomingMovies"];
       let { errorMessage, moviesData, searchTerm } = this.props;
       const values = queryString.parse(searchTerm);
-      //console.log(values.query, typeof values.query);
+
       // checks if values.query comes from SearchBar component or LandingPage component
       if (!searchArray.includes(values.query)) {
         // values.query comes from SearchBar component
