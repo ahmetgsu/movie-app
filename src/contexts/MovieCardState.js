e6f187a22d42f2f23bf1a4b17fb607deabffdfba
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-22 00:37:14 +0300

    MovieCardState cleaned up

--- src/contexts/MovieCardState.js
+++ src/contexts/MovieCardState.js
@@ -17,13 +17,6 @@ import {
 } from "./types";
 
 const MovieCardStore = props => {
-  //   const [activeIndex, setActiveIndex] = useState(0);
-  //   const [iconClicked, setIconClicked] = useState(false);
-  //   const [modalOpen, setModalOpen] = useState(false);
-  //   const [isHovered, setIsHovered] = useState(false);
-  //   const [activeIndexRate, setActiveIndexRate] = useState(null);
-  //   const [starIndex, setStarIndex] = useState(null);
-
   const initialState = {
     activeIndex: 0,
     iconClicked: false,
@@ -36,44 +29,17 @@ const MovieCardStore = props => {
 
   const [state, dispatch] = useReducer(movieCardReducer, initialState);
 
-  // USE WITH JUST HOOKS (WITHOUT REDUCER)
-  //   const handleClick = videoNumber => {
-  //     setActiveIndex(videoNumber);
-  //   };
-
-  // USE WITH REDUCER
   const handleClick = videoNumber =>
     dispatch({ type: VIDEO_BUTTON_CLICK, payload: videoNumber });
 
-  // USE WITH JUST HOOKS (WITHOUT REDUCER)
-  //   const handleIconClick = () => {
-  //     if (iconClicked === false) {
-  //       setIconClicked(!iconClicked);
-  //     } else {
-  //       setIconClicked(!iconClicked);
-  //     }
-  //   };
-
-  // USE WITH REDUCER
   const handleIconClick = () => {
-    const { iconClicked } = state;
-    if (iconClicked === false) {
-      dispatch({ type: WATCHLIST_ICON_CLICK, payload: !iconClicked });
+    if (state.iconClicked === false) {
+      dispatch({ type: WATCHLIST_ICON_CLICK, payload: !state.iconClicked });
     } else {
-      dispatch({ type: WATCHLIST_ICON_CLICK, payload: !iconClicked });
+      dispatch({ type: WATCHLIST_ICON_CLICK, payload: !state.iconClicked });
     }
   };
 
-  // USE WITH JUST HOOKS (WITHOUT REDUCER)
-  //   const handleClickStar = index => {
-  //     if (starIndex === null) {
-  //       setStarIndex(index);
-  //     } else {
-  //       setStarIndex(index);
-  //     }
-  //   };
-
-  // USE WITH REDUCER
   const handleClickStar = index => {
     if (state.starIndex === null) {
       dispatch({ type: STAR_CLICK, payload: index });
@@ -82,62 +48,17 @@ const MovieCardStore = props => {
     }
   };
 
-  //   const handleClickTimes = () => {
-  //     setStarIndex(null);
-  //   };
-
-  // USE WITH REDUCER
   const handleClickTimes = () => dispatch({ type: DELETE_MOVIE_RATE_CLICK });
 
-  //   const handleOpen = () => {
-  //     setModalOpen(true);
-  //   };
-
-  // USE WITH REDUCER
   const handleOpen = () => dispatch({ type: MODAL_OPEN });
 
-  //   const handleClose = () => {
-  //     setModalOpen(false);
-  //   };
-
-  // USE WITH REDUCER
   const handleClose = () => dispatch({ type: MODAL_CLOSE });
 
-  //   const handleMouseOver = index => {
-  //     setIsHovered(true);
-  //     setActiveIndexRate(index);
-  //   };
-
-  // USE WITH REDUCER
   const handleMouseOver = index =>
     dispatch({ type: MOUSE_OVER, payload: index });
 
-  //   const handleMouseOut = () => {
-  //     setIsHovered(false);
-  //   };
-
-  // USE WITH REDUCER
   const handleMouseOut = () => dispatch({ type: MOUSE_OUT });
 
-  //   const fetchMovieRate = async movieId => {
-  //     console.log(
-  //       "fetchMovieRate from context is invoked with movieId: ",
-  //       movieId
-  //     );
-  //     const res1 = await movieUserActions.get("/movieRates");
-  //     const id = _.find(
-  //       res1.data,
-  //       item => item.movieId === parseInt(movieId, 10)
-  //     );
-  //     if (id !== undefined) {
-  //       const response = await movieUserActions.get(`/movieRates/${id.id}`);
-  //       setStarIndex(response.data.userRate);
-  //     } else {
-  //       return;
-  //     }
-  //   };
-
-  // USE WITH REDUCER
   const fetchMovieRate = async movieId => {
     console.log(
       "fetchMovieRate from context is invoked with movieId: ",
@@ -156,24 +77,6 @@ const MovieCardStore = props => {
     }
   };
 
-  //   const movieWatchlistCheck = async movieId => {
-  //     console.log(
-  //       "movieWatchlistCheck from context is invoked with movieId: ",
-  //       movieId
-  //     );
-  //     const res1 = await movieUserActions.get("/watchlist");
-  //     const id = _.find(
-  //       res1.data,
-  //       item => item.movieId === parseInt(movieId, 10)
-  //     );
-  //     if (id !== undefined) {
-  //       setIconClicked(true);
-  //     } else {
-  //       return;
-  //     }
-  //   };
-
-  // USE WITH REDUCER
   const movieWatchlistCheck = async movieId => {
     console.log(
       "movieWatchlistCheck from context is invoked with movieId: ",
