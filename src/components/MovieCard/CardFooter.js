bc979f9eb7c36d91ccdedb76d115c1e67452102f
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-22 22:41:50 +0300

    redundant lines deleted after hooks integration

--- src/components/MovieCard/CardFooter.js
+++ src/components/MovieCard/CardFooter.js
@@ -27,8 +27,8 @@ const CardFooter = () => {
         <span style={{ color: "dodgerblue" }}>
           <strong>See full cast and crew</strong>
           {/* <FullCastModal
-              selectedMovieCredits={selectedMovieCredits}
-              selectedMovieData={selectedMovieData}
+              credits={credits}
+              movieData={movieData}
             /> */}
         </span>
       </Grid.Row>
@@ -37,46 +37,3 @@ const CardFooter = () => {
 };
 
 export default CardFooter;
-
-// import React from "react";
-// import { connect } from "react-redux";
-// import { Grid, Card } from "semantic-ui-react";
-
-// const CardFooter = props => {
-//   const { credits /*selectedMovieData*/ } = props;
-//   return (
-//     <Card.Content>
-//       <Grid.Row>
-//         <strong>Director: </strong>
-//         {/* to handle error in case of no crew information */}
-//         {credits.crew.length !== 0
-//           ? credits.crew.find(item => item.job === "Director").name
-//           : ""}
-//       </Grid.Row>
-//       <Grid.Row>
-//         <strong>Stars: </strong>
-//         {credits.cast.slice(0, 5).map((item, index) => (
-//           <span key={index}>
-//             <strong>{` ${item.name} `}</strong>
-//             {index === 4 ? ` as ${item.character}` : ` as ${item.character}, `}
-//           </span>
-//         ))}
-//         <br />
-//         <span style={{ color: "dodgerblue" }}>
-//           <strong>See full cast and crew</strong>
-//           {/* <FullCastModal
-//               selectedMovieCredits={selectedMovieCredits}
-//               selectedMovieData={selectedMovieData}
-//             /> */}
-//         </span>
-//       </Grid.Row>
-//     </Card.Content>
-//   );
-// };
-
-// const mapStateToProps = state => ({
-//   credits: state.movies.selectedMovieCredits,
-//   movieData: state.movies.selectedMovieData
-// });
-
-// export default connect(mapStateToProps)(CardFooter);
