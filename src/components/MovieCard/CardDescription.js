0b7679abc66fb27356a5f7cf98de7276572a1e7e
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-22 22:38:24 +0300

    redundant lines deleted after hooks

--- src/components/MovieCard/CardDescription.js
+++ src/components/MovieCard/CardDescription.js
@@ -14,26 +14,3 @@ const CardDescription = () => {
 };
 
 export default CardDescription;
-
-// REACT_REDUX STYLE
-
-// import React from "react";
-// import { connect } from "react-redux";
-// import { Card } from "semantic-ui-react";
-
-// const CardDescription = props => {
-//   const { movieData } = props;
-//   return (
-//     <Card.Content>
-//       <Card.Description style={{ fontSize: "18px" }}>
-//         {movieData.overview}
-//       </Card.Description>
-//     </Card.Content>
-//   );
-// };
-
-// const mapStateToProps = state => ({
-//   movieData: state.movies.selectedMovieData
-// });
-
-// export default connect(mapStateToProps)(CardDescription);
