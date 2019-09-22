9bd95586355a340c6072d1ded9e37fde76d64bd6
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-23 00:44:28 +0300

    Hooks integrated to RateMovieText component

--- src/components/MovieCard/RateMovieText.js
+++ src/components/MovieCard/RateMovieText.js
@@ -1,29 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Context from "../../contexts/movieCardContext";
 
-class RateMovieText extends React.Component {
-  static contextType = Context;
+const RateMovieText = () => {
+  const { starIndex } = useContext(Context);
 
-  render() {
-    const { starIndex } = this.context;
-
-    return starIndex === null ? (
-      <p style={{ fontSize: "19px" }}>Rate movie</p>
-    ) : (
-      <>
-        <h2 style={{ margin: "0px", fontWeight: "bold" }}>{starIndex}</h2>
-        <h4
-          style={{
-            fontSize: "13px",
-            marginTop: "2px",
-            marginLeft: "5px"
-          }}
-        >
-          Your Rate
-        </h4>
-      </>
-    );
-  }
-}
+  return starIndex === null ? (
+    <p style={{ fontSize: "19px" }}>Rate movie</p>
+  ) : (
+    <>
+      <h2 style={{ margin: "0px", fontWeight: "bold" }}>{starIndex}</h2>
+      <h4
+        style={{
+          fontSize: "13px",
+          marginTop: "2px",
+          marginLeft: "5px"
+        }}
+      >
+        Your Rate
+      </h4>
+    </>
+  );
+};
 
 export default RateMovieText;
