0395d581bd6bc2463791576c71dc7cb19200062e
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-22 22:17:22 +0300

    Hooks integrated to WatchListPopUp component

--- src/components/MovieCard/WatchListPopUp.js
+++ src/components/MovieCard/WatchListPopUp.js
@@ -1,62 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
 import Context from "../../contexts/movieCardContext";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToWatchList, deleteFromWatchList } from "../../actions/userActions";
 import { Popup, Icon } from "semantic-ui-react";
 
-class WatchListPopUp extends React.Component {
-  static contextType = Context;
+const WatchListPopUp = () => {
+  //Redux related hooks
+  const dispatch = useDispatch();
+  const addItem = movieId => dispatch(addToWatchList(movieId));
+  const deleteItem = movieId => dispatch(deleteFromWatchList(movieId));
 
-  iconCondition = movieId => {
-    const { iconClicked, handleIconClick } = this.context;
-    const { isSignedIn } = this.props;
+  const isSignedIn = useSelector(state => state.auth.isSignedIn);
+  const movieData = useSelector(state => state.movies.selectedMovieData);
+
+  // Context related hooks
+  const context = useContext(Context);
+  const { iconClicked, handleIconClick } = context;
+
+  const iconCondition = movieId => {
     return isSignedIn
       ? iconClicked === false
-        ? (handleIconClick(), this.props.addToWatchList(movieId))
-        : (handleIconClick(), this.props.deleteFromWatchList(movieId))
+        ? (handleIconClick(), addItem(movieId))
+        : (handleIconClick(), deleteItem(movieId))
       : alert("Please sign in first to add movie to your watchlist");
   };
 
-  render() {
-    const { movieData } = this.props;
-    const { iconClicked } = this.context;
-    return (
-      <Popup
-        content={
-          iconClicked === false
-            ? "+ Add movie to watchlist"
-            : "Movie successfully added to watchlist"
-        }
-        hoverable
-        inverted
-        flowing
-        position="bottom left"
-        style={{
-          opacity: 0.9,
-          border: "5px #33112c",
-          padding: "0.8em",
-          fontWeight: "bold"
-        }}
-        trigger={
-          <Icon
-            name={iconClicked === false ? "plus square" : "check square"}
-            size="huge"
-            color={iconClicked === false ? "grey" : "green"}
-            link
-            onClick={() => this.iconCondition(movieData.id)}
-          />
-        }
-      />
-    );
-  }
-}
+  return (
+    <Popup
+      content={
+        iconClicked === false
+          ? "+ Add movie to watchlist"
+          : "Movie successfully added to watchlist"
+      }
+      hoverable
+      inverted
+      flowing
+      position="bottom left"
+      style={{
+        opacity: 0.9,
+        border: "5px #33112c",
+        padding: "0.8em",
+        fontWeight: "bold"
+      }}
+      trigger={
+        <Icon
+          name={iconClicked === false ? "plus square" : "check square"}
+          size="huge"
+          color={iconClicked === false ? "grey" : "green"}
+          link
+          onClick={() => iconCondition(movieData.id)}
+        />
+      }
+    />
+  );
+};
 
-const mapStateToProps = state => ({
-  movieData: state.movies.selectedMovieData,
-  isSignedIn: state.auth.isSignedIn
-});
-
-export default connect(
-  mapStateToProps,
-  { addToWatchList, deleteFromWatchList }
-)(WatchListPopUp);
+export default WatchListPopUp;
