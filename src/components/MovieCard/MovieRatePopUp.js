a51479e5d96db499454471b9c35a275596c71c2a
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-22 23:20:28 +0300

    Hooks integrated and helper function created

--- src/components/MovieCard/MovieRatePopUp.js
+++ src/components/MovieCard/MovieRatePopUp.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import RateMovieText from "./RateMovieText";
 import {
   createMovieRate,
@@ -10,81 +10,78 @@ import Context from "../../contexts/movieCardContext";
 import { Popup, Icon, Menu } from "semantic-ui-react";
 import _ from "lodash";
 
-class MovieRatePopUp extends React.Component {
-  static contextType = Context;
+const MovieRatePopUp = () => {
+  const context = useContext(Context);
+  const { isHovered, activeIndexRate, starIndex, handleClickStar } = context;
+  const dispatch = useDispatch();
+  const createRate = (id, i) => dispatch(createMovieRate(id, i));
+  const updateRate = (id, i) => dispatch(updateMovieRate(id, i));
+  const deleteRate = id => dispatch(deleteMovieRate(id));
+  const movieData = useSelector(state => state.movies.selectedMovieData);
+  const isSignedIn = useSelector(state => state.auth.isSignedIn);
 
-  starCondition = (movieId, index) => {
-    const { starIndex, handleClickStar } = this.context;
-    const { isSignedIn } = this.props;
+  const starCondition = (movieId, index) => {
     return isSignedIn
       ? starIndex === null
-        ? (handleClickStar(index), this.props.createMovieRate(movieId, index))
-        : (handleClickStar(index), this.props.updateMovieRate(movieId, index))
+        ? (handleClickStar(index), createRate(movieId, index))
+        : (handleClickStar(index), updateRate(movieId, index))
       : alert("To rate this movie, you need to sign in first");
   };
 
-  render() {
-    const { isHovered, activeIndexRate, starIndex } = this.context;
-    const { movieData } = this.props;
-    return (
-      <Popup
-        flowing
-        position="left center"
-        hoverable
-        inverted
-        content={_.times(11, i =>
-          i === 0 ? (
-            <Icon
-              onMouseOver={() => this.context.handleMouseOver(i)}
-              onMouseOut={this.context.handleMouseOut}
-              onClick={() => {
-                this.context.handleClickTimes();
-                this.props.deleteMovieRate(movieData.id);
-              }}
-              name={
-                isHovered && activeIndexRate === i
-                  ? "times circle outline"
-                  : "times circle"
-              }
-              color={isHovered && activeIndexRate === i ? "red" : null} //if 2 conditions right, it's red, otherwise null
-              key={i}
-              size="large"
-              style={{ marginRight: "10px" }}
-            />
-          ) : (
-            <Icon
-              onMouseOver={() => this.context.handleMouseOver(i)}
-              onMouseOut={this.context.handleMouseOut}
-              onClick={() => this.starCondition(movieData.id, i)}
-              name={isHovered && i <= activeIndexRate ? "star" : "star outline"}
-              color={isHovered && i <= activeIndexRate ? "yellow" : null}
-              key={i}
-              size="large"
-              style={{ marginleft: "6px", marginRight: "6px" }}
-            />
-          )
-        )}
-        trigger={
-          <Menu.Item style={{ margin: "0px" }}>
-            <Icon
-              name={starIndex === null ? "star outline" : "star"}
-              color={starIndex === null ? null : "yellow"}
-              size="large"
-            />
-            <RateMovieText />
-          </Menu.Item>
-        }
-      />
+  const popupContent = () => {
+    return _.times(11, i =>
+      i === 0 ? (
+        <Icon
+          onMouseOver={() => context.handleMouseOver(i)}
+          onMouseOut={context.handleMouseOut}
+          onClick={() => {
+            context.handleClickTimes();
+            deleteRate(movieData.id);
+          }}
+          name={
+            isHovered && activeIndexRate === i
+              ? "times circle outline"
+              : "times circle"
+          }
+          color={isHovered && activeIndexRate === i ? "red" : null} //if 2 conditions right, it's red, otherwise null
+          key={i}
+          size="large"
+          style={{ marginRight: "10px" }}
+        />
+      ) : (
+        <Icon
+          onMouseOver={() => context.handleMouseOver(i)}
+          onMouseOut={context.handleMouseOut}
+          onClick={() => starCondition(movieData.id, i)}
+          name={isHovered && i <= activeIndexRate ? "star" : "star outline"}
+          color={isHovered && i <= activeIndexRate ? "yellow" : null}
+          key={i}
+          size="large"
+          style={{ marginleft: "6px", marginRight: "6px" }}
+        />
+      )
     );
-  }
-}
+  };
 
-const mapStateToProps = state => ({
-  movieData: state.movies.selectedMovieData,
-  isSignedIn: state.auth.isSignedIn
-});
+  return (
+    <Popup
+      flowing
+      position="left center"
+      hoverable
+      inverted
+      content={popupContent()}
+      trigger={
+        <Menu.Item style={{ margin: "0px" }}>
+          <Icon
+            name={starIndex === null ? "star outline" : "star"}
+            color={starIndex === null ? null : "yellow"}
+            size="large"
+          />
+          <RateMovieText />
+        </Menu.Item>
+      }
+    />
+  );
+};
 
-export default connect(
-  mapStateToProps,
-  { createMovieRate, updateMovieRate, deleteMovieRate }
-)(MovieRatePopUp);
+export default MovieRatePopUp;
