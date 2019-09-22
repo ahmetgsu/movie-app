4119fdf3fba17d783146975f382ddaee8c77ce5f
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-23 00:59:39 +0300

    Hooks integrated to App.js, useRef hooks used

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React, { useRef } from "react";
 import { Router, Route } from "react-router-dom";
 import MoviesContainer from "./MoviesContainer";
 import SearchBar from "./SearchBar/SearchBar";
@@ -8,33 +8,24 @@ import LandingPage from "./LandingPage/LandingPage";
 import history from "../history";
 import { Sticky } from "semantic-ui-react";
 
-class App extends React.Component {
-  contextRef = createRef();
+const App = () => {
+  const contextRef = useRef();
 
-  render() {
-    return (
-      <div
-        className="ui container"
-        style={{ width: "100%" }}
-        ref={this.contextRef}
-      >
-        <Router history={history}>
-          <div>
-            <Sticky context={this.contextRef}>
-              <SearchBar />
-            </Sticky>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/movies/list" component={MoviesList} />
-            <Route
-              path="/movies/:movieId/details"
-              component={MoviesContainer}
-            />
-            <Footer />
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui container" style={{ width: "100%" }} ref={contextRef}>
+      <Router history={history}>
+        <div>
+          <Sticky context={contextRef}>
+            <SearchBar />
+          </Sticky>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/movies/list" component={MoviesList} />
+          <Route path="/movies/:movieId/details" component={MoviesContainer} />
+          <Footer />
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
