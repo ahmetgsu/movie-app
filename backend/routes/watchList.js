b88abf2445136ff6ae4255eb8aff547edd08b695
ahmetgsu <ahmet.gsu@gmail.com>
2019-10-16 23:52:47 +0300

    auth middleware, Watchlist Model created, get watchlists and add watchlist routes works

--- backend/routes/watchList.js
+++ backend/routes/watchList.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+// const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Watchlist = require('../models/Watchlist');
 
 // @route   GET api/watchlist
 // @desc    Get all users watchlisted movies
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all watchlisted movies of specific user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const watchlistedMovies = await Watchlist.find({ user: req.user.id });
+    res.json(watchlistedMovies);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/watchlist
 // @desc    Add new movie to watchlist
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add new movie to watchlist of a user');
+router.post('/', auth, async (req, res) => {
+  const { movieId, movieTitle } = req.body;
+  try {
+    const newMovie = new Watchlist({
+      movieId,
+      movieTitle,
+      user: req.user.id
+    });
+
+    const movie = await newMovie.save();
+    res.json(movie);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   DELETE api/watchlist/:id
